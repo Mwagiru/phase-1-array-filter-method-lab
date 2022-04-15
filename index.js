a4e5df5d52a0648588eb9e4915669c97ad4ed055
Mwagiru <joymwagiru@gmail.com>
2022-04-15 12:59:25 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(array, String){
+  return array.filter((array)=> array.toUpperCase()=== String.toUpperCase());
+}
+let drivers = ["Andrew", "Ken", "Raymond","Robert","Ramon"]
+function fuzzyMatch(array, String) {
+  return array.filter((driver)=> driver.toUpperCase().indexOf(String.toUpperCase()) === 0
+  );
+}
+function matchName(array, String){
+  return array.filter((record) => record.name === String);
+}
